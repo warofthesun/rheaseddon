5df69d4d10131a94d2f2f8f0e6c4524e1ede17b8
Daniel Brown <fireflip@gmail.com>
2017-12-29 12:35:06 -0600

    Videos have minimal chrome

--- page-contact.php
+++ page-contact.php
@@ -30,8 +30,41 @@ if( have_rows('video_clips') ):
  	// loop through the rows of data
     while ( have_rows('video_clips') ) : the_row();
 
-        // display a sub field value
-        the_sub_field('video_link');
+        
+
+
+
+				// get iframe HTML
+				$iframe = get_sub_field('video_link');
+
+
+				// use preg_match to find iframe src
+				preg_match('/src="(.+?)"/', $iframe, $matches);
+				$src = $matches[1];
+
+
+				// add extra params to iframe src
+				$params = array(
+				'showinfo'    => 0,
+				'hd'        => 1,
+				'autohide'    => 1
+				);
+
+				$new_src = add_query_arg($params, $src);
+
+				$iframe = str_replace($src, $new_src, $iframe);
+
+
+				// add extra attributes to iframe html
+				$attributes = 'showinfo=0';
+
+				$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+
+				// echo $iframe
+				echo $iframe;
+
+
 
     endwhile;
 
@@ -42,39 +75,7 @@ else :
 endif;
 
 ?>
-<?php
 
-// get iframe HTML
-$iframe = get_field('video_link');
-
-
-// use preg_match to find iframe src
-preg_match('/src="(.+?)"/', $iframe, $matches);
-$src = $matches[1];
-
-
-// add extra params to iframe src
-$params = array(
-'showinfo'    => 0,
-'hd'        => 1,
-'autohide'    => 1
-);
-
-$new_src = add_query_arg($params, $src);
-
-$iframe = str_replace($src, $new_src, $iframe);
-
-
-// add extra attributes to iframe html
-$attributes = 'showinfo=0';
-
-$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
-
-
-// echo $iframe
-echo $iframe;
-
-?>
 	</div>
 </div> <!-- end #content_area -->
 
